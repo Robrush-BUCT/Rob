58c61b50a389d1fe02bbf8734cd79e7062464d76
LiangGuang <48551522+Lgmx666666@users.noreply.github.com>
2021-07-10 19:03:54 +0800

    Add files via upload

--- liangguang-code/object_detection/run.py
+++ liangguang-code/object_detection/run.py
@@ -6,14 +6,12 @@ import cv2
 class RUN():
     def __init__(self):
         self.runcode = 2
-        self.OBJECT_DETECT = object_detect.OBJECT_DETECT('./pic/round2-1.jpg')
+        self.OBJECT_DETECT = object_detect.OBJECT_DETECT()
         self.video = cv2.VideoCapture(0)
 
-    def pass_hole(self):
+    def pass_hole(self, video_img):
         while self.runcode == 2:
-            ret, frame = self.video.read()
-            print(frame.shape)
-            theta, right_dist, areaS = self.OBJECT_DETECT.hole_path_detect(frame)
+            theta, right_dist, areaS = self.OBJECT_DETECT.hole_path_detect(video_img)
             if areaS >= 20000:
                 if right_dist >= 100:
                     print("机器人右平移")
@@ -29,6 +27,17 @@ class RUN():
                 print("Passed  Hole Successfully")
                 self.runcode = 3
 
+    def test_pass_hole(self):
+        for i in range(6):
+            raw_img = cv2.imread("./pic/2-{}.jpg".format(i+1))
+            theta, right_dist, areaS = self.OBJECT_DETECT.hole_path_detect(raw_img)
+            print("-------pic{} start--------".format(i+1))
+            print("theta", theta)
+            print("right_dist", right_dist)
+            print("areaS", areaS)
+            print("-------pic{} end--------".format(i + 1))
+            cv2.waitKey(0)
+
 if __name__ == "__main__":
     main_mission = RUN()
-    main_mission.pass_hole()
+    main_mission.test_pass_hole()
